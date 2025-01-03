04dfead63cac9de8b668c768ea2892f5d02c8754
root <root@NULL>
2025-01-03 17:40:12 +0800

    First commit, with libft, minilibx_linux, srcs and header file. Managed to get mlx printing out imgs.

---
+++ srcs/init.c
@@ -0,0 +1,13 @@
+// #include "so_long.h"
+// #include "mlx.h"
+
+// void    init_so_long(t_data *data)
+// {
+//     data -> mlx.win_size.x = 1980;
+//     data -> mlx.win_size.y = 1080;
+
+//     fdf->camera.zoom = 11;
+//     fdf->camera.y_angle = 30;
+//     fdf->camera.x_offset = fdf->mlx.win_size.x / 2;
+//     fdf->camera.y_offset = fdf->mlx.win_size.y / 2;
+// }
